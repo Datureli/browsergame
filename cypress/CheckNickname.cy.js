84991f9b3a62a007fd62c7f388f185ff3c67632e
Datureli <soleris1@gmail.com>
2024-01-21 14:16:18 +0100

    implement page object model for nickname tests

--- cypress/CheckNickname.cy.js
+++ cypress/CheckNickname.cy.js
@@ -1,3 +1,5 @@
+// home.spec.js
+import homePage from "./pages/NickaneObjectModel";
 import Home from "../src/views/Home.vue";
 
 describe("Strona główna", () => {
@@ -5,30 +7,19 @@ describe("Strona główna", () => {
     cy.mount(Home);
   });
 
-  it("Check if random name generation is working", () => {
-    cy.get("button:contains('losowe')").click();
-
-    cy.get("input").should("not.have.value", "");
+  it("Sprawdź, czy losowa generacja nazwy działa", () => {
+    homePage.checkIfRandomNameGenerated();
   });
 
-  it("Check if you can pass to character choose with empty spaces or tabs", () => {
-    cy.get("input")
-      .type("   ")
-      .should("not.have.value", "");
+  it("Sprawdź, czy można przejść do wyboru postaci z pustymi odstępami lub tabulatorami", () => {
+    homePage.checkIfEmptySpacesOrTabsNotAllowed();
   });
 
-  it("Check if the 'Continue' button has the 'activeLink' class when the 'nickname' field is longer than 5 characters", () => {
-    cy.get("#nicknameInput").type("pięćLiter");
-
-    cy.get("button:contains('Continue')").should('have.class', 'activeStatus');
-
-    cy.get("#nicknameInput").clear();
-    cy.get("button:contains('Continue')").should('not.have.class', 'activeStatus');
+  it("Sprawdź, czy przycisk 'Continue' ma klasę 'activeLink' po wprowadzeniu więcej niż 5 znaków w polu 'nickname'", () => {
+    homePage.checkIfContinueButtonHasActiveLinkClass();
   });
 
-  it("Should check if the input does not contain special characters", () => {
-    cy.get('input').type('??--////``++--');
-
-    cy.get("input").invoke('val').should('match', /^[a-zA-Z0-9]*$/);
+  it("Sprawdź, czy pole nie zawiera znaków specjalnych", () => {
+    homePage.checkIfInputDoesNotContainSpecialCharacters();
   });
 });
--- cypress/pages/NickaneObjectModel.js
+++ cypress/pages/NickaneObjectModel.js
@@ -1,14 +1,42 @@
-class nicknamePage {
-    get nicknameField() {
-        return cy.get("#nicknameInput")
+class HomePage {
+    clickRandomNameButton() {
+      cy.get("button:contains('losowe')").click();
     }
-
-    get submitNickname() {
-        return cy.get("")
+  
+    inputField() {
+      return cy.get("input");
     }
-
-    fillNickname(nickname) {
-        this.nicknameField.type(nickname)
+  
+    continueButton() {
+      return cy.get("button:contains('Continue')");
     }
-}
-export default nicknamePage;
+  
+    getNicknameInput() {
+      return cy.get("#nicknameInput");
+    }
+  
+    checkIfRandomNameGenerated() {
+      this.clickRandomNameButton();
+      this.inputField().should("not.have.value", "");
+    }
+  
+    checkIfEmptySpacesOrTabsNotAllowed() {
+      this.inputField().type("   ").should("not.have.value", "");
+    }
+  
+    checkIfContinueButtonHasActiveLinkClass() {
+      this.getNicknameInput().type("pięćLiter");
+      this.continueButton().should('have.class', 'activeStatus');
+  
+      this.getNicknameInput().clear();
+      this.continueButton().should('not.have.class', 'activeStatus');
+    }
+  
+    checkIfInputDoesNotContainSpecialCharacters() {
+      this.inputField().type('??--////``++--');
+      this.inputField().invoke('val').should('match', /^[a-zA-Z0-9]*$/);
+    }
+  }
+  
+  export default new HomePage();
+  
