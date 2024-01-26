335aede745ee05a18ac96f14c1d378b46517c4fe
Datureli <soleris1@gmail.com>
2024-01-26 20:01:22 +0100

    fix cookie modal problem on fb

--- cypress/e2e/allegro/facebook.cy.js
+++ cypress/e2e/allegro/facebook.cy.js
@@ -1,10 +1,16 @@
 describe("Test strony MMA.pl", () => {
-  before(() => {
+  beforeEach(() => {
     cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
     cy.visit("https://facebook.com");
+
+    cy.get('.selected[data-cookiebanner="accept_button"]').should("be.visible").click();
   });
 
   it("Przescrolluj na dół formularza i kliknij w przycisk", () => {
     cy.get('.selected[data-cookiebanner="accept_button"]').click();
   });
+
+  it("try logging in", () => {
+    cy.get("#email").type("username20");
+  })
 });
