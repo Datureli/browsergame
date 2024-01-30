2e8d38d85f7ff8b093669efecf6f8d8f1befba18
Datureli <soleris1@gmail.com>
2024-01-30 17:12:54 +0100

    add assertion for quick payment data test

--- cypress/e2e/websites/demo-bank/desktop.cy.js
+++ cypress/e2e/websites/demo-bank/desktop.cy.js
@@ -3,12 +3,13 @@ describe("test user desktop", () => {
     cy.visit("https://demobank.jaktestowac.pl/pulpit.html");
   });
 
-  it("check fast transfer", () => {
+  it("quick payments data", () => {
     cy.get("#widget_1_transfer_receiver").select(1);
-    cy.get("#widget_1_transfer_amount").type(11.20);
+    cy.get("#widget_1_transfer_amount").clear().type(11.20);
     cy.get("#widget_1_transfer_title").type("Przelew za książki");
 
     cy.get("#execute_btn").click();
 
+    cy.get("#show_messages").should("be.visible").should('have.text', 'Przelew wykonany! Jan Demobankowy - 11,20PLN - zasileniePrzelew za książki');
   });
 });
