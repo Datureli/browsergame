32af3d47dd5e80766c75004124314ac6fa01cfa1
Datureli <soleris1@gmail.com>
2024-02-04 19:32:48 +0100

    fix counter test

---
+++ cypress/e2e/websites/demo-bank/desktop-sessionTime.cy.js
@@ -0,0 +1,25 @@
+describe("test session time after succesfull log in", () => {
+  beforeEach(() => {
+    cy.visit("https://demobank.jaktestowac.pl/pulpit.html");
+  });
+
+  it("Check if counter is equal to 10 minutes", () => {
+    cy.get("#session_time").should("be.visible");
+    cy.get("#countdown_minutes").invoke("text").should("eq", "10");
+  });
+
+  it("Check if the counter will count down 10 seconds from the initial value", () => {
+    cy.get("#session_time").should("be.visible");
+
+    const secondsToCheck = 10;
+
+    for (let i = 0; i < secondsToCheck; i++) {
+      cy.wait(1000);
+  
+      cy.get('#countdown_seconds').invoke('text').then((currentValue) => {
+        const expectedValue = 59 - i;
+        expect(currentValue).to.eq(expectedValue.toString());
+      });
+    }
+  });
+});
