084c70de3019401e147d6032f4163313d435976f
Datureli <soleris1@gmail.com>
2024-02-10 15:19:53 +0100

    fix iframe tests

--- cypress/e2e/websites/demo-bank/dashboard/reports.cy.js
+++ cypress/e2e/websites/demo-bank/dashboard/reports.cy.js
@@ -1,9 +1,13 @@
 describe("test report section", () => {
-    beforeEach(() => {
-        cy.visit("https://demobank.jaktestowac.pl/reports-iframe.html");
-    })
+  beforeEach(() => {
+    cy.visit("https://demobank.jaktestowac.pl/reports-iframe.html");
+  });
 
-    it("Check if i can download txt file", () => {
+  it("Check if i can download txt file", () => {
+    cy.get("iframe").then((iframe) => {
+      cy.wrap(iframe.contents()).within(() => {
         cy.get("#btn_dl_txt").click();
-    })
-})
+      });
+    });
+  });
+});
