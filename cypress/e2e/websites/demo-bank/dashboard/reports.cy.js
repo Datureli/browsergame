f13e93d0dc8368a2b6292a4ac3043043deabd63e
Datureli <soleris1@gmail.com>
2024-02-12 19:46:06 +0100

    improve iframe test for reports

--- cypress/e2e/websites/demo-bank/dashboard/reports.cy.js
+++ cypress/e2e/websites/demo-bank/dashboard/reports.cy.js
@@ -1,15 +1,25 @@
 describe("test report section", () => {
+  let iframe;
+
   beforeEach(() => {
     cy.visit("https://demobank.jaktestowac.pl/reports-iframe.html");
+    cy.get("iframe").then((frame) => {
+      iframe = frame.contents();
+    });
   });
 
-  it("Check if i can download txt file", () => {
-    cy.get("iframe").then((iframe) => {
-      cy.wrap(iframe.contents()).within(() => {
-        cy.get("#btn_dl_txt").click();
-        cy.intercept("GET", "**/proposed_file_name.txt").as("download");
-        cy.readFile('C:/Users/48578/Downloads/proposed_file_name.txt');
-      });
+  const downloadFileTest = (buttonId, fileExtension) => {
+    cy.wrap(iframe).within(() => {
+      cy.get(buttonId).click();
+      cy.readFile(`C:/Users/48578/Downloads/proposed_file_name.${fileExtension}`);
     });
+  };
+
+  it("Check if i can download txt file", () => {
+    downloadFileTest("#btn_dl_txt", "txt");
+  });
+
+  it("Check if i can download zip file", () => {
+    downloadFileTest("#btn_dl_zip", "zip");
   });
 });
