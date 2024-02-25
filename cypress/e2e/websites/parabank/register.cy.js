2af5b1e2b15c1baeed64233cbcd4dee43b6b83f6
Datureli <soleris1@gmail.com>
2024-02-25 17:06:29 +0100

    add assertion for creating account test

--- cypress/e2e/websites/parabank/register.cy.js
+++ cypress/e2e/websites/parabank/register.cy.js
@@ -7,15 +7,20 @@ describe("register new account", () => {
     cy.get("#loginPanel a:contains('Register')").click();
 
     cy.get('[name="customer.firstName"]').type("Pawel");
-    cy.get('[name="customer.lastName"]').type("Pawel");
-    cy.get('[name="customer.address.street"]').type("Pawel");
-    cy.get('[name="customer.address.city"]').type("Pawel");
-    cy.get('[name="customer.address.state"]').type("Pawel");
-    cy.get('[name="customer.address.zipCode"]').type("Pawel");
-    cy.get('[name="customer.phoneNumber"]').type("Pawel");
-    cy.get('[name="customer.ssn"]').type("Pawel");
-    cy.get('[name="customer.username"]').type("Pawel");
-    cy.get('[name="customer.password"]').type("Pawel");
-    cy.get('[name="repeatedPassword"]').type("Pawel");
+    cy.get('[name="customer.lastName"]').type("Niewiadomski");
+    cy.get('[name="customer.address.street"]').type("Juniorów 4");
+    cy.get('[name="customer.address.city"]').type("Nibylandia");
+    cy.get('[name="customer.address.state"]').type("taki jak w reakcie");
+    cy.get('[name="customer.address.zipCode"]').type("211");
+    cy.get('[name="customer.phoneNumber"]').type("07000881");
+    cy.get('[name="customer.ssn"]').type("pypypy");
+    cy.get('[name="customer.username"]').type("Datureli11");
+    cy.get('[name="customer.password"]').type("Pawel123");
+    cy.get('[name="repeatedPassword"]').type("Pawel123");
+
+    cy.get('input[type="submit"][value="Register"]').click();
+
+    cy.get('p').contains('Your account was created successfully. You are now logged in.').should('be.visible');
+
   });
 });
