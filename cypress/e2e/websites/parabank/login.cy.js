e100f3b9a6dd0fb4f6380195d3b48f7af7598976
Datureli <soleris1@gmail.com>
2024-02-28 12:51:21 +0100

    fix login tests

--- cypress/e2e/websites/parabank/login.cy.js
+++ cypress/e2e/websites/parabank/login.cy.js
@@ -6,6 +6,12 @@ describe("test bank login", () => {
   it("test if i can login with inccorect credentials", () => {
     cy.get('#loginPanel input[type="text"]').type("dasdsa");
     cy.get('#loginPanel input[type="password"]').type("password1");
+
+    cy.get('input[type="submit"]').click();
+
+    cy.get(".error")
+      .should("be.visible")
+      .contains("An internal error has occurred and has been logged.");
   });
 
   it("Login with correct credentials", () => {
@@ -15,8 +21,8 @@ describe("test bank login", () => {
     cy.get('input[type="submit"]').click();
 
     cy.get(".smallText")
-    .should("be.visible")
-    .invoke("text")
-    .should("include", "Welcome John Smith");
+      .should("be.visible")
+      .invoke("text")
+      .should("include", "Welcome John Smith");
   });
 });
