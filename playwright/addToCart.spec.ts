4af212ce99a137e8648b9ddf55d83d625cb88dbb
Datureli <soleris1@gmail.com>
2024-03-01 20:19:23 +0100

    add assertion to text, fix wairForUrl for firefox browser by removing timeout

--- playwright/addToCart.spec.ts
+++ playwright/addToCart.spec.ts
@@ -6,7 +6,13 @@ test("Try add item to cart", async ({ page }) => {
   );
 
   await page.getByRole("link", { name: "Echo Fit Compression Short" }).first().click();
-  await page.waitForURL("**/echo-fit-compression-short.html", { timeout: 5000 });
+  await page.waitForURL("**/echo-fit-compression-short.html");
+  await page.click("#option-label-size-143-item-171");
+  await page.click("#option-label-color-93-item-49");
 
   await page.getByRole("button", { name: "Add to Cart" }).click();
+
+  await page.waitForSelector('.counter-number');
+  const counterNumber = await page.innerText('.counter-number');
+  expect(counterNumber).toBe('1');
 });
