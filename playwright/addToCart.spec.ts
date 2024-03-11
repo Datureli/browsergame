f2fdca4682558d165661dd50c56b5c848d976e7f
Datureli <soleris1@gmail.com>
2024-03-11 11:32:03 +0100

    fix check if item has been added to cart test

--- playwright/addToCart.spec.ts
+++ playwright/addToCart.spec.ts
@@ -28,28 +28,17 @@ test("check if item has been added to cart", async ({ page }) => {
     "https://magento.softwaretestingboard.com/collections/yoga-new.html"
   );
 
-  await page
-    .getByRole("link", { name: "Echo Fit Compression Short" })
-    .first()
-    .click();
+  await page.locator('li').filter({ hasText: 'Echo Fit Compression Short' }).locator('button').click();
   await page.waitForURL("**/echo-fit-compression-short.html");
-  await page.click("#option-label-size-143-item-171");
-  await page.click("#option-label-color-93-item-49");
+  await page.getByLabel('28').click();
+  await page.getByLabel('Black').click();
 
   await page.getByRole("button", { name: "Add to Cart" }).click();
-  await page.getByText("My Cart").click();
-  await page.getByText("View and Edit Cart").click();
+  await page.getByRole('link', { name: ' My Cart 1 1\nitems' }).click();
+  await page.getByRole('link', { name: 'View and Edit Cart' }).click();
+  await page.goto('https://magento.softwaretestingboard.com/checkout/#shipping');
 
-  await page.waitForURL("**/checkout/cart/");
-
-  await page.waitForSelector(
-    'td.col.item a[title="Echo Fit Compression Short"]'
-  );
-
-  // Możesz również sprawdzić, czy element jest widoczny, korzystając z isVisible
-  const isProductVisible = await page.isVisible(
-    'td.col.item a[title="Echo Fit Compression Short"]'
-  );
+  const isProductVisible = await page.getByRole('cell', { name: 'Echo Fit Compression Short' }).isVisible();
   expect(isProductVisible).toBe(true);
 });
 
