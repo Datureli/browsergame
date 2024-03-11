bace07c16a3553c1737bed0e6bebdf4844be383f
Datureli <soleris1@gmail.com>
2024-03-11 15:44:02 +0100

    fix test: Check if the item has been added to the cart - by improving url waiting

--- playwright/addToCart.spec.ts
+++ playwright/addToCart.spec.ts
@@ -23,43 +23,58 @@ test("Check if cart quantity changing after add item to cart", async ({
   expect(counterNumber).toBe("1");
 });
 
-test("check if item has been added to cart", async ({ page }) => {
+test("Check if the item has been added to the cart", async ({ page }) => {
   await page.goto(
     "https://magento.softwaretestingboard.com/collections/yoga-new.html"
   );
 
-  await page.locator('li').filter({ hasText: 'Echo Fit Compression Short' }).locator('button').click();
+  await page
+    .getByRole("link", { name: "Echo Fit Compression Short" })
+    .first()
+    .click();
   await page.waitForURL("**/echo-fit-compression-short.html");
-  await page.getByLabel('28').click();
-  await page.getByLabel('Black').click();
+  await page.getByLabel("28").click();
+  await page.getByLabel("Black").click();
 
   await page.getByRole("button", { name: "Add to Cart" }).click();
-  await page.getByRole('link', { name: ' My Cart 1 1\nitems' }).click();
-  await page.getByRole('link', { name: 'View and Edit Cart' }).click();
-  await page.goto('https://magento.softwaretestingboard.com/checkout/#shipping');
+  await page.getByRole("link", { name: " My Cart 1 1\nitems" }).click();
+  await page.getByRole("link", { name: "View and Edit Cart" }).click();
+  await page.goto(
+    "https://magento.softwaretestingboard.com/checkout/cart/"
+  );
+  await page.waitForURL("**/checkout/cart/");
 
-  const isProductVisible = await page.getByRole('cell', { name: 'Echo Fit Compression Short' }).isVisible();
-  expect(isProductVisible).toBe(true);
+  const isProductVisible = await page
+    .getByRole("cell", { name: "Echo Fit Compression Short" })
+    .isVisible();
+ await expect(isProductVisible).toBe(true);
 });
 
 test("check if the cart icon opens a modal window", async ({ page }) => {
   await page.goto("https://magento.softwaretestingboard.com/");
-  await page.getByRole('link', { name: 'My Cart' }).click();
-  
-  await expect(page.locator('#ui-id-1')).toBeVisible();
+  await page.getByRole("link", { name: "My Cart" }).click();
+
+  await expect(page.locator("#ui-id-1")).toBeVisible();
 });
 
-test("Check if there is information that the basket is empty", async ({ page }) => {
+test("Check if there is information that the basket is empty", async ({
+  page,
+}) => {
   await page.goto("https://magento.softwaretestingboard.com/");
-  await page.getByRole('link', { name: 'My Cart' }).click();
+  await page.getByRole("link", { name: "My Cart" }).click();
 
-  await expect(page.getByText('You have no items in your shopping cart.')).toBeVisible();
-})
+  await expect(
+    page.getByText("You have no items in your shopping cart.")
+  ).toBeVisible();
+});
 
-test("check if the active class is added to it after opening the basket icon", async ({ page }) => {
+test("check if the active class is added to it after opening the basket icon", async ({
+  page,
+}) => {
   await page.goto("https://magento.softwaretestingboard.com/");
-  await page.getByRole('link', { name: 'My Cart' }).click();
+  await page.getByRole("link", { name: "My Cart" }).click();
 
-  await expect(page.getByRole('link', { name: 'My Cart' })).toHaveClass(/active/);
-
-})
+  await expect(page.getByRole("link", { name: "My Cart" })).toHaveClass(
+    /active/
+  );
+});
