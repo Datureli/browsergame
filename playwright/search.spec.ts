6d7ec90dfa28f00fbf82e22d6a1ae1e2938fec88
Datureli <soleris1@gmail.com>
2024-04-06 14:10:14 +0200

    add new test for searchbar

--- playwright/search.spec.ts
+++ playwright/search.spec.ts
@@ -1,10 +1,24 @@
 import { test, expect } from "@playwright/test";
 
-test("Check if the word search result appears with the selected word", async ({ page }) => {
+test("Check if the word search result appears with the selected word", async ({
+  page,
+}) => {
   await page.goto("https://magento.softwaretestingboard.com/");
 
   await page.getByPlaceholder("Search entire store here...").fill("yoga");
   await page.getByPlaceholder("Search entire store here...").press("Enter");
-  
-  await expect(page.getByRole('heading', { name: 'Search results for: \'yoga\'' }).locator('span')).toBeVisible();
+
+  await expect(
+    page
+      .getByRole("heading", { name: "Search results for: 'yoga'" })
+      .locator("span")
+  ).toBeVisible();
+});
+
+test("Check if there is a message stating that the search phrase must have at least 3 letters", async ({ page }) => {
+  await page.goto("https://magento.softwaretestingboard.com/");
+  await page.getByPlaceholder("Search entire store here...").fill("?");
+  await page.getByPlaceholder("Search entire store here...").press("Enter");
+
+  await expect(page.getByText("Minimum Search query length is 31")).toBeVisible();
 });
