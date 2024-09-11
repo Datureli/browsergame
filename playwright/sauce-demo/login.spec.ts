4925ab14ef8e17c2684c1c65bda084aa9ee4713c
Datureli <soleris1@gmail.com>
2024-09-11 10:07:51 +0200

    add new test - Check login with incorrect credential

--- playwright/sauce-demo/login.spec.ts
+++ playwright/sauce-demo/login.spec.ts
@@ -9,3 +9,17 @@ test("Check if the correct login redirects the user to the dashboard", async ({ 
 
   await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
 });
+
+test("Check login with incorrect credential", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+
+  await page.locator('[data-test="username"]').fill("new_user");
+  await page.locator('[data-test="password"]').fill("secret_sauce");
+  await page.locator('[data-test="login-button"]').click();
+
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+})
+
+test("test wygaśniecia sesji", async ({ page }) => {
+  
+})
