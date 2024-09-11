a9804c887050769e8e6117e3b4a649ba4d223f29
Datureli <soleris1@gmail.com>
2024-09-11 12:09:10 +0200

    add beforeEach hook before every test

--- playwright/sauce-demo/login.spec.ts
+++ playwright/sauce-demo/login.spec.ts
@@ -1,8 +1,11 @@
 import { test, expect } from "@playwright/test";
 
-test("Check if the correct login redirects the user to the dashboard", async ({ page }) => {
+// Użyj funkcji beforeEach, aby przed każdym testem otworzyć stronę
+test.beforeEach(async ({ page }) => {
   await page.goto("https://www.saucedemo.com/");
+});
 
+test("Check if the correct login redirects the user to the dashboard", async ({ page }) => {
   await page.locator('[data-test="username"]').fill("standard_user");
   await page.locator('[data-test="password"]').fill("secret_sauce");
   await page.locator('[data-test="login-button"]').click();
@@ -11,15 +14,21 @@ test("Check if the correct login redirects the user to the dashboard", async ({ 
 });
 
 test("Check login with incorrect credential", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/");
-
   await page.locator('[data-test="username"]').fill("new_user");
   await page.locator('[data-test="password"]').fill("secret_sauce");
   await page.locator('[data-test="login-button"]').click();
 
   await expect(page.locator('[data-test="error"]')).toBeVisible();
-})
+});
 
-test("test wygaśniecia sesji", async ({ page }) => {
-  
-})
+test("Check login function with correct credential", async ({ page }) => {
+  await page.locator('[data-test="username"]').fill("standard_user");
+  await page.locator('[data-test="password"]').fill("secret_sauce");
+  await page.locator('[data-test="login-button"]').click();
+
+  await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+});
+
+test("Test wygaśnięcia sesji", async ({ page }) => {
+ 
+});
