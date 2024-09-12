3addf5e077eed6f0fdc162d06e4b5bc3e7d10af9
Datureli <soleris1@gmail.com>
2024-09-12 18:20:08 +0200

    add new test - Check whether the dashboard is displayed correctly for a user with a slow Internet connection

--- playwright/sauce-demo/login.spec.ts
+++ playwright/sauce-demo/login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-// Użyj funkcji beforeEach, aby przed każdym testem otworzyć stronę
+
 test.beforeEach(async ({ page }) => {
   await page.goto("https://www.saucedemo.com/");
 });
@@ -48,33 +48,58 @@ test("Attempt to log in with an empty user field", async ({ page }) => {
   );
 });
 
-test("Attempt to log in with an empty password field", async ({ page}) => {
+test("Attempt to log in with an empty password field", async ({ page }) => {
   await page.locator('[data-test="username"]').fill("standard_user");
   await page.locator('[data-test="login-button"]').click();
 
-  await expect(page.locator('[data-test="error"]')).toHaveText("Epic sadface: Password is required")
-})
+  await expect(page.locator('[data-test="error"]')).toHaveText(
+    "Epic sadface: Password is required"
+  );
+});
 
-test("Attempt to log in with a blocked user's account", async ({ page}) => {
+test("Attempt to log in with a blocked user's account", async ({ page }) => {
   await page.locator('[data-test="username"]').fill("locked_out_user");
   await page.locator('[data-test="password"]').fill("secret_sauce");
 
   await page.locator('[data-test="login-button"]').click();
 
-  await expect(page.locator('[data-test="error"]')).toHaveText("Epic sadface: Sorry, this user has been locked out.")
-})
+  await expect(page.locator('[data-test="error"]')).toHaveText(
+    "Epic sadface: Sorry, this user has been locked out."
+  );
+});
 
-test("Checking whether logging in with the problematic user still triggers the error that the photo thumbnail has been changed", async ({ page}) => {
+test("Checking whether logging in with the problematic user still triggers the error that the photo thumbnail has been changed", async ({
+  page,
+}) => {
   await page.locator('[data-test="username"]').fill("problem_user");
   await page.locator('[data-test="password"]').fill("secret_sauce");
 
   await page.locator('[data-test="login-button"]').click();
 
-  const thumbnailSrc = await page.locator('[data-test="item-4-img-link"] img').getAttribute('src');
+  const thumbnailSrc = await page
+    .locator('[data-test="item-4-img-link"] img')
+    .getAttribute("src");
 
   await page.locator('[data-test="item-4-img-link"]').click();
 
-  const detailsSrc = await page.locator('[data-test="item-sauce-labs-fleece-jacket-img"]').getAttribute('src');
- 
+  const detailsSrc = await page
+    .locator('[data-test="item-sauce-labs-fleece-jacket-img"]')
+    .getAttribute("src");
+
   expect(thumbnailSrc).toBe(detailsSrc);
-})
+});
+
+
+test("Check whether the dashboard is displayed correctly for a user with a slow Internet connection", async ({ page }) => {
+  await page.locator('[data-test="username"]').fill("performance_glitch_user");
+  await page.locator('[data-test="password"]').fill("secret_sauce");
+
+  await page.locator('[data-test="login-button"]').click();
+
+  await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+
+  await expect(page.locator('[data-test="inventory-container"]')).toBeVisible();
+
+  await expect(page.locator('.shopping_cart_link')).toBeVisible();
+});
+
