03d16f8c317175b9cac169469e808016378ea526
Datureli <soleris1@gmail.com>
2024-09-14 19:26:54 +0200

    add new test -Whether the item added in the store is the same in the basket

--- playwright/sauce-demo/Cart.spec.ts
+++ playwright/sauce-demo/Cart.spec.ts
@@ -1,39 +1,58 @@
 import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
-    await page.locator('[data-test="username"]').fill("standard_user");
-    await page.locator('[data-test="password"]').fill("secret_sauce");
-    await page.locator('[data-test="login-button"]').click();
+  await page.goto("https://www.saucedemo.com/");
+  await page.locator('[data-test="username"]').fill("standard_user");
+  await page.locator('[data-test="password"]').fill("secret_sauce");
+  await page.locator('[data-test="login-button"]').click();
 });
 
 test("Add product to cart", async ({ page }) => {
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
 
-  const cartCount = await page.locator('.shopping_cart_badge').innerText();
-  expect(cartCount).toBe('1');
+  const cartCount = await page.locator(".shopping_cart_badge").innerText();
+  expect(cartCount).toBe("1");
 });
 
 test("Remove product from cart", async ({ page }) => {
-    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-  
-    let cartCount = await page.locator('.shopping_cart_badge').innerText();
-    expect(cartCount).toBe('1');
-  
-    await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
-  
-    const cartBadge = await page.locator('.shopping_cart_badge').count();
-    expect(cartBadge).toBe(0);
-});
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
 
+  let cartCount = await page.locator(".shopping_cart_badge").innerText();
+  expect(cartCount).toBe("1");
+
+  await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+
+  const cartBadge = await page.locator(".shopping_cart_badge").count();
+  expect(cartBadge).toBe(0);
+});
 
 test("Add two or more items to cart", async ({ page }) => {
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
-  await page.locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
+  await page
+    .locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]')
+    .click();
+  await page
+    .locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]')
+    .click();
   await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
-  
-  const cartCount = await page.locator('.shopping_cart_badge').innerText();
-  expect(cartCount).toBe('4');
-})
 
+  const cartCount = await page.locator(".shopping_cart_badge").innerText();
+  expect(cartCount).toBe("4");
+});
+
+test("Whether the item added in the store is the same in the basket", async ({ page }) => {
+  const itemName = await page
+    .locator('[data-test="inventory-item-name"]')
+    .first()
+    .innerText();
+
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+  await page.locator('[data-test="shopping-cart-link"]').click();
+
+  const cartItemName = await page
+    .locator(".cart_item .inventory_item_name")
+    .innerText();
+
+  expect(itemName).toBe(cartItemName);
+});
